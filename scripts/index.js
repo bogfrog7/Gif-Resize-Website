83f0908f00af18b5255f03e5213649437e3d3eec
Bogdan Bas <iharbas@yahoo.com>
2022-01-15 22:40:21 -0800

    added source files

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,30 @@
 const gitButton = document.getElementById("githubButton");
 const input = document.getElementById("fileInput");
+
 let started = false
 
-input.onchange = () => {
-  const reader = new FileReader();
+const RenderImage = (image, reader) => {
+  image.onload = () => {
+    image.height = 200
+    image.width = 200
+  }
+  image.src = reader.result
+  let container = document.getElementById("imageBox");
+  container.appendChild(image)
+}
 
+input.onchange = () => {
+  console.log(" |log| Loaded in a file")
+  const reader = new FileReader();
   reader.onload = () => {
     const image = new Image()
-    image.onload = () => {
-      image.height = 200
-      image.width = 200
-    }
-    image.src = reader.result
-    let container = document.getElementById("imageBox");
+
     if (input.files[0]["type"] == "image/gif"){
-        container.appendChild(image)
+        RenderImage(image, reader);
+        input.remove();
     }else{
       alert("The file type has to be a gif");
+      console.warn("|log|'The file must be a gif'")
     }
   };
   reader.readAsDataURL(input.files[0])
