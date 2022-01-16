da8f11a713ac79c9ea86e699459745d99dfdafed
Bogdan Bas <iharbas@yahoo.com>
2022-01-15 21:54:19 -0800

    added source files

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,27 @@
-const githubButton = document.getElementById("githubButton");
-const fileInput = document.getElementById("fileInput")
+const gitButton = document.getElementById("githubButton");
+const input = document.getElementById("fileInput");
+let started = false
 
-function readFile(){
-    let reader = new FileReader();
+input.onchange = () => {
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    const image = new Image()
+    image.onload = () => {
+      image.height = 200
+      image.width = 200
+    }
+    image.src = reader.result
+    let container = document.getElementById("imageBox");
+    if (input.files[0]["type"] == "image/gif"){
+        container.appendChild(image)
+    }else{
+      alert("The file type has to be a gif");
+    }
+  };
+  reader.readAsDataURL(input.files[0])
 
 }
-
-githubButton.onclick = () => {
+gitButton.onclick = () => {
   window.open("https://github.com");
 };
